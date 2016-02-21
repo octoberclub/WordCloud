9a2e07f43512caf3d56cc6133159f3b1df97829a
Michelle Steele <octoberclub@gmail.com>
2016-02-21 16:12:17 +0000

    Add jqcloud to display a wordcloud. Read topics.json and serve minimal json to the client

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 var express = require('express')
+var fs = require('fs');
 var app = express();
+
+function readJSONFile(filename, callback) {
+  fs.readFile(filename, function (err, data) {
+    if(err) {
+      callback(err);
+      return;
+    }
+    try {
+      callback(null, JSON.parse(data));
+    } catch(exception) {
+      callback(exception);
+    }
+  });
+}
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/wordcloud.json', function (req, res) {
+
+  readJSONFile('topics.json', function (err, json) {
+  
+    var words = [];
+
+    for (var i in json.topics) {
+      words[words.length]={
+	  "text": json.topics[i].label,
+          "weight": json.topics[i].volume 
+      };
+    }
+
+    res.json(JSON.stringify(words));
+
+  });
 });
 
 app.listen(3000, function () {
--- public/js/wordcloud-app.js
+++ public/js/wordcloud-app.js
@@ -1,17 +1,8 @@
 var main = function () {
-  $.getJSON("topics.json", function (response) {
-    $("body").append("<p> topic label count : " + response.topics.length + "</p>");
-
-
-        var output="<ul>";
-        for (var i in response.topics) {
-            output+="<li>" + response.topics[i].label+"</li>";
-        }
-
-        output+="</ul>";
-        document.getElementById("nav").innerHTML=output;
- 
+  $.getJSON("wordcloud.json", function (response) {
+    console.log("res=", response);
+     $("#wordcloud").jQCloud( JSON.parse(response) );
   });
 };
 
-$( document ).ready(main);
+$(document).ready(main);
